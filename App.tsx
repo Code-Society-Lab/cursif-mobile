c05d137cfce529de033a45c00b59918942dd5e3b
Chris <rolletchris1@gmail.com>
2023-08-23 20:41:12 -0400

    added dark mode

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import styles from './styles';
 
 export default function App() {
+  const [isBlackBackground, setIsBlackBackground] = useState(true);
+
+  const toggleBackground = () => {
+    setIsBlackBackground(!isBlackBackground);
+  };
+
+  const containerStyle = [
+    styles.container,
+    isBlackBackground ? { backgroundColor: 'black' } : { backgroundColor: 'white' },
+  ];
+
+  const textStyle = [
+    styles.text,
+    isBlackBackground ? { color: 'white' } : { color: 'black' },
+  ];
+
+  const cornerTextStyle = [
+    styles.cornerText,
+    isBlackBackground ? styles.whiteCornerText : styles.blackCornerText,
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text><h1>Welcome to Cursif!</h1></Text>
+    <View style={containerStyle}>
+      <TouchableOpacity onPress={toggleBackground}>
+        <Text style={textStyle}>Welcome to Cursif!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={toggleBackground} style={cornerTextStyle}>
+        <Text style={isBlackBackground ? { color: 'white' } : {}}>{isBlackBackground ? 'White' : 'Black'}</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
